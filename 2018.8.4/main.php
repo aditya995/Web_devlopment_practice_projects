d39e4fc32d343701bc21adcfeeca450218f36df3
aditya995 <39437959+aditya995@users.noreply.github.com>
2018-08-04 04:19:02 +0600

    .

--- 2018.8.4/main.php
+++ 2018.8.4/main.php
@@ -1,11 +1,22 @@
 <?php
 	// taking the contents from .json file
 	$info = file_get_contents('info.json');
+	$dogs = file_get_contents('dogs.json');
 	//Decoding the json infos into array
 	//Getting a json data by >>  file_get_contents(file_name.json);
 	//Finally taking Informations as php array using json_decode($variable);
 	
-	$decode = json_decode($info);
-	print_r($decode);
+	$decode1 = json_decode($info);
+	$decode2 = json_decode($dogs);
+	
+	echo'Writing the first json file infos <br /><pre>';
+	
+	print_r($decode1);
+	
+	echo'</pre><br />Writing the second json file infos<br /><pre>';
+	
+	print_r($decode2);
+	
+	echo'</pre><br />';
 	
 ?>
